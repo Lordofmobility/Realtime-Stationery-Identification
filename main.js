b1c7544f97fbfd0d73a80e1e9714be9235f12fd7
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-03-12 20:31:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 function draw() {
-
+    image(video, 0, 0, 250, 250)
+    classifier.classify(video, gotResult)
 }
 
 function setup() {
     canvas = createCanvas(250, 250);
     canvas.center()
+    video = createCapture(VIDEO)
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/O1cyoACUi/model.json", modelLoaded)
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(results);
+        document.getElementById("result_object").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
